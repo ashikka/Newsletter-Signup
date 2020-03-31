7daa505b9b7f387a259b7ca81495f9b2f54bca40
Rohan Mukherjee <rohanbayern@gmail.com>
2020-03-31 21:38:07 +0530

    feat: Changed dotenv/types to dotenv

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
 const dotenv = require("dotenv");
+
 dotenv.config();
 
 const app = express();
